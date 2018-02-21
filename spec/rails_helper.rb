cdd99ec805f55467db947ab5845dc8ed7242ed30
Kristian Garza <kj.garza@gmail.com>
2018-02-21 04:00:22 +0100

    rspec framework

---
+++ spec/rails_helper.rb
@@ -0,0 +1,55 @@
+ENV['RAILS_ENV'] = 'test'
+
+# set up Code Climate
+require 'simplecov'
+SimpleCov.start
+
+require File.expand_path('../../config/environment', __FILE__)
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+require "rspec/rails"
+require "shoulda-matchers"
+require "webmock/rspec"
+require "rack/test"
+require "colorize"
+require "database_cleaner"
+require "sidekiq/testing"
+
+# Checks for pending migration and applies them before tests are run.
+ActiveRecord::Migration.maintain_test_schema!
+
+WebMock.disable_net_connect!(
+  allow: ['codeclimate.com:443', ENV['PRIVATE_IP']],
+  allow_localhost: true
+)
+
+# configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+RSpec.configure do |config|
+  # add `FactoryBot` methods
+  config.include FactoryBot::Syntax::Methods
+
+  # don't use transactions, use database_clear gem via support file
+  config.use_transactional_fixtures = false
+
+  # add custom json method
+  config.include RequestSpecHelper, type: :request
+
+  ActiveJob::Base.queue_adapter = :test
+
+end
+
+VCR.configure do |c|
+  c.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  c.hook_into :webmock
+  c.ignore_localhost = true
+  c.ignore_hosts "codeclimate.com"
+  c.configure_rspec_metadata!
+end
