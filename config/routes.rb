70cd9ece050376c1b387f1d999eb6e1a9eb8fd9e
Kristian Garza <kj.garza@gmail.com>
2018-02-21 03:15:32 +0100

    basic routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :heartbeat, only: [:index]
+  resources :index, path: '/', only: [:index]
+  resources :status, only: [:index]
 end
