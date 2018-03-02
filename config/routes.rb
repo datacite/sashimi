5f93d2d68adac83acc4ecccd7de506c0013ee9a0
Kristian Garza <kj.garza@gmail.com>
2018-03-02 13:58:17 +0900

    generation of the app core

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,21 @@ Rails.application.routes.draw do
   resources :heartbeat, only: [:index]
   resources :index, path: '/', only: [:index]
   resources :status, only: [:index]
+
+  def add_swagger_route http_method, path, opts = {}
+    full_path = path.gsub(/{(.*?)}/, ':\1')
+    match full_path, to: "#{opts.fetch(:controller_name)}##{opts[:action_name]}", via: http_method
+  end
+
+  add_swagger_route 'GET', '//publishers', controller_name: 'publishers', action_name: 'index'
+  add_swagger_route 'DELETE', '//publishers/{id}', controller_name: 'publishers', action_name: 'destroy'
+  add_swagger_route 'GET', '//publishers/{id}', controller_name: 'publishers', action_name: 'show'
+  add_swagger_route 'PUT', '//publishers/{id}', controller_name: 'publishers', action_name: 'update'
+  add_swagger_route 'POST', '//publishers', controller_name: 'publishers', action_name: 'create'
+  add_swagger_route 'GET', '//report_types', controller_name: 'report_types', action_name: 'index'
+  add_swagger_route 'GET', '//reports', controller_name: 'reports', action_name: 'index'
+  add_swagger_route 'DELETE', '//reports/{id}', controller_name: 'reports', action_name: 'destroy'
+  add_swagger_route 'GET', '//reports/{id}', controller_name: 'reports', action_name: 'show'
+  add_swagger_route 'PUT', '//reports/{id}', controller_name: 'reports', action_name: 'update'
+  add_swagger_route 'POST', '//reports', controller_name: 'reports', action_name: 'create'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,61 @@
 
 ActiveRecord::Schema.define(version: 0) do
 
+  create_table "error_models", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.integer "code"
+    t.string "severity"
+    t.string "message"
+    t.string "help_url"
+    t.string "cata"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "publishers", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.string "publisher_name"
+    t.string "publisher_id"
+    t.string "path"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "report_types", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.string "report_id"
+    t.string "release"
+    t.string "report_description"
+    t.string "path"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reports", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.string "id"
+    t.string "report_name"
+    t.string "report_id"
+    t.string "release"
+    t.string "created"
+    t.string "created_by"
+    t.string "report_datasets"
+    t.string "exceptions"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "status_alerts", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.string "date_time"
+    t.string "alert"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "statuses", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.string "description"
+    t.boolean "service_active"
+    t.string "registry_url"
+    t.string "note"
+    t.string "alerts"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
