2ac9946d433e1b6adc1777e9f9b7442aad1c6c0a
Kristian Garza <kj.garza@gmail.com>
2018-03-15 00:55:46 +0100

    report_header

--- app/serializers/report_serializer.rb
+++ app/serializers/report_serializer.rb
@@ -1,9 +1,18 @@
 class ReportSerializer < ActiveModel::Serializer
   type 'report'
 
-  attributes :id, :report_name, :report_id, :created_by, :report_datasets
+  attributes  :id, :report_header, :report_datasets
 
   def id
     object.uid
+  end
+
+  def report_header
+    {
+      :report_name => object.report_name,
+      :report_id => object.report_id,
+      :created_by => object.created_by,
+      :created => object.created
+    }
   end
 end
