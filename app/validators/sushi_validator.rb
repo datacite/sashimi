3da87def6b5288ca7c6fb2e1ad3e857901ece2c6
Kristian Garza <kj.garza@gmail.com>
2018-04-13 13:03:54 +0200

    nice errors

--- app/validators/sushi_validator.rb
+++ app/validators/sushi_validator.rb
@@ -2,7 +2,19 @@ class SushiValidator < ActiveModel::EachValidator
 
   def validate_each(record, _schema, _value)
     unless  record.is_valid_sushi? 
-      record.errors["sushi_format"] << (record.validate_sushi || "Your SUSHI is wrong mate!!")
+
+      def dig_errors(errors)
+        return [] if errors.nil?
+      
+        if errors.is_a? Hash
+          [ errors.dig(:message) ]
+        else
+          errors.map { |error| {error.dig(:fragment) => error.dig(:message)}  }
+        end
+      end
+      nice_erros = dig_errors record.validate_sushi
+
+      record.errors["errors"] << (nice_erros || "Your SUSHI is wrong mate!!")
     end
   end
 end
