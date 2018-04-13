0dc1d74dbb9c59810747f4fcb44c9ba3b233acf1
Kristian Garza <kj.garza@gmail.com>
2018-04-13 12:03:38 +0200

    kebab_case standarisation trought-out

--- config/initializers/active_model_serializers.rb
+++ config/initializers/active_model_serializers.rb
@@ -4,6 +4,9 @@ ActiveModelSerializers.config.include_data_default = :if_sideloaded
 # disable pagination links as it requires expensive count queries on the database
 ActiveModelSerializers.config.jsonapi_pagination_links_enabled = false
 
+ActiveModel::Serializer.config.key_transform = :dash
+ActiveModel::Serializer.config.default_includes = "**"
+
 ActiveSupport.on_load(:action_controller) do
   require 'active_model_serializers/register_jsonapi_renderer'
 end
