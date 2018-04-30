a403a049c9ad4c12e1350dfbff82f1ae69caa3f2
Kristian Garza <kj.garza@gmail.com>
2018-04-30 11:42:19 +0200

    configuration of the queue
    connected datacite/mastino#13

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
-
+  config.active_job.queue_adapter = :inline
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
