efd96c874ae32382df7be2895ab2c7836fe97de2
Kristian Garza <kj.garza@gmail.com>
2018-04-30 18:40:17 +0200

    spec for job

--- spec/concerns/queueable_spec.rb
+++ spec/concerns/queueable_spec.rb
@@ -3,24 +3,14 @@ require 'rails_helper'
 describe Report, vcr: true do
   subject { create(:report) }
 
-  context "register_report" do
+  context "queue_report" do
     let(:client) { create(:client) }
 
-    subject { build(:report, report: "10.5438/mcnv-ga6n", client: client) }
+    subject { build(:report) }
 
-    it 'should register' do
-      options = { url: "https://blog.datacite.org/re3data-science-europe/", username: ENV['MDS_USERNAME'], password: ENV['MDS_PASSWORD'] }
-      expect(subject.register_url(options).body).to eq("data"=>"OK")
-    end
-
-    it 'missing username and password' do
-      options = { url: "https://blog.datacite.org/re3data-science-europe/" }
-      expect(subject.register_url(options).body).to eq("errors"=>[{"title"=>"Username or password missing"}])
-    end
-
-    it 'wrong username and password' do
-      options = { url: "https://blog.datacite.org/re3data-science-europe/", username: ENV['MDS_USERNAME'], password: 12345 }
-      expect(subject.register_url(options).body).to eq("errors"=>[{"status"=>401, "title"=>"Unauthorized"}])
+    it 'should queue' do
+      options = {}
+      expect(subject.queue_report(options).body).to eq("data"=>"OK")
     end
   end
 end
