83045de165c4fde878cc595c179bae50a28f7256
Kristian Garza <kj.garza@gmail.com>
2018-05-02 15:50:32 +0200

    improving testing

--- spec/concerns/queueable_spec.rb
+++ spec/concerns/queueable_spec.rb
@@ -12,5 +12,9 @@ describe Report, vcr: true do
       options = {}
       # expect(subject.queue_report(options)).to eq({"queue_url":"test_usage","message_body":{"report_id":"dsr-12hd-zt1"},"message_attributes":{"report-id":{"string_value":"dsr-12hd-zt1","data_type":"String"}}}.to_s)
     end
+
+    it 'report_url' do
+      expect(subject.report_url).to eq("https://metrics.test.datacite.org/reports/#{subject.report_id}")
+    end
   end
 end
