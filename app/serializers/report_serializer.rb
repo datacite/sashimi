a75a365db5f395977e93132feea26da3c6682be6
Kristian Garza <kj.garza@gmail.com>
2018-05-22 15:18:57 +0200

    added extra fields to report to show

--- app/serializers/report_serializer.rb
+++ app/serializers/report_serializer.rb
@@ -11,8 +11,12 @@ class ReportSerializer < ActiveModel::Serializer
     {
       :report_name => object.report_name,
       :report_id => object.report_id,
+      :release => object.release,
       :created_by => object.created_by,
-      :created => object.created
+      :created => object.created,
+      :reporting_period => object.reporting_period, 
+      :report_filters=> object.report_filters, 
+      :report_attributes => object.report_attributes
     }
   end
 end
