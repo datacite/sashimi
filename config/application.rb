e47e6a485745f088941f49f6789df948311c9536
Kristian Garza <kj.garza@gmail.com>
2018-06-12 09:03:15 +0200

    change url for usage

--- config/application.rb
+++ config/application.rb
@@ -51,7 +51,7 @@ ENV['MYSQL_HOST'] ||= "mysql"
 ENV['MYSQL_PORT'] ||= "3306"
 ENV['TRUSTED_IP'] ||= "10.0.40.1"
 ENV['LEVRIERO_URL'] ||= "https://api.datacite.org"
-ENV['USAGE_URL'] ||= "https://metrics.datacite.org"
+ENV['USAGE_URL'] ||= "https://api.datacite.org"
 
 
 module Sashimi
