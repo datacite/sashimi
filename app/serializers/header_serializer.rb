163d86034362fbf9a61d9340a2d301bf211822af
Kristian Garza <kj.garza@gmail.com>
2018-06-20 06:55:16 +0200

    show only header for faster request

---
+++ app/serializers/header_serializer.rb
@@ -0,0 +1,23 @@
+class HeaderSerializer < ActiveModel::Serializer
+  type 'report'
+
+  attributes  :id, :report_header
+
+  def id
+    object.uid
+  end
+
+  def report_header
+    {
+      :report_name => object.report_name,
+      :report_id => object.report_id,
+      :release => object.release,
+      :created_by => object.created_by,
+      :created => object.created,
+      :reporting_period => object.reporting_period, 
+      :report_filters=> object.report_filters, 
+      :report_attributes => object.report_attributes
+    }
+  end
+
+end
