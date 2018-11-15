adae6342aecad128f4650c14891904cf4350def1
Kristian Garza <kj.garza@gmail.com>
2018-11-15 18:49:16 +0100

    more efficient parsing of json

--- lib/middleware/compressed_requests.rb
+++ lib/middleware/compressed_requests.rb
@@ -1,3 +1,5 @@
+require 'yajl'
+
 class CompressedRequests
   def initialize(app)
     @app = app
@@ -12,13 +14,28 @@ class CompressedRequests
   end
 
   def call(env)
+    request = Rack::Request.new(env)
     if method_handled?(env) && encoding_handled?(env)
       extracted = decode(env['rack.input'], env['HTTP_CONTENT_ENCODING'])
+      # request.update_param('report', JSON.parse(extracted).to_json )
+      parser = Yajl::Parser.new
+      hsh = parser.parse(extracted)
+      # hsh = JSON.parse(extracted)
+      puts "This is PARSED"
+
+      request.update_param('report_header', hsh.fetch("report-header",{}).deep_transform_keys { |key| key.tr('-', '_') } )
+      puts "This is header"
+
+      request.update_param('report_datasets', hsh.fetch("report-datasets",{}))
+      puts "This is body"
+
+      request.update_param('encoding', env['HTTP_CONTENT_ENCODING'])
 
       env.delete('HTTP_CONTENT_ENCODING')
       env['CONTENT_LENGTH'] = extracted.length
       env['rack.input'] = StringIO.new(extracted)
     end
+
 
     status, headers, response = @app.call(env)
     return [status, headers, response]
