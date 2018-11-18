85cdcb3ed047c55c7b95e98c6512c7f41b624340
Kristian Garza <kj.garza@gmail.com>
2018-11-18 14:57:52 +0100

    remove key trasnformations

--- config/initializers/json_param_key_transform.rb
+++ config/initializers/json_param_key_transform.rb
@@ -6,7 +6,11 @@ ActionDispatch::Request.parameter_parsers[:json] = -> (raw_post) {
   data = {:_json => data} unless data.is_a?(Hash)
 
   # Transform camelCase param keys to snake_case:
-  data.deep_transform_keys!(&:underscore)
-  data.deep_transform_keys! { |key| key.underscore }
+  # data.deep_transform_keys!(&:underscore)
+  # data.deep_transform_keys! { |key| key.underscore }
 
+  data.transform_keys! { |key| key.underscore }
+  header = data.fetch("report_header").deep_transform_keys { |key| key.underscore }
+  data["report_header"] = header
+  data
 }
