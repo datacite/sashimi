f286f001aa6c97c1a05af906c11e4018ef688cf9
Kristian Garza <kj.garza@gmail.com>
2018-11-19 13:06:09 +0100

    added new factory

--- spec/factories/default.rb
+++ spec/factories/default.rb
@@ -8,6 +8,7 @@ FactoryBot.define do
     created "2020-03-02"
     reporting_period  "begin_date": "2018-03-01", "end_date": "2018-03-31" 
     report_filters []
+    release "rd1"
     report_attributes []
     exceptions []
     report_datasets [{
@@ -39,6 +40,45 @@ FactoryBot.define do
             {
               "access-method": "regular",
               "metric-type": "unique-dataset-investigations",
+              "count": 3
+            }
+          ]
+        }
+      ]
+    }]
+  end
+
+  factory :resolution_report do
+    sequence(:report_id) { |n| "dsr-12hd-zt#{n}" }
+    client_id "datacite.datacite"
+    provider_id "datacite"
+    sequence(:created_by) { |n| "datacite#{n}" }
+    created "2020-03-02"
+    reporting_period  "begin_date": "2018-03-01", "end_date": "2018-03-31" 
+    report_filters []
+    release "drl"
+    report_attributes []
+    exceptions []
+    report_datasets [{
+      "dataset-id": [{
+          "type": "doi",
+          "value": "10.5072/0931-865"
+      }],
+      "performance": [
+        {
+          "period": {
+            "begin-date": "2018-03-01",
+            "end-date": "2018-03-31"
+          },
+          "instance": [
+            {
+              "access-method": "regular",
+              "metric-type": "total-resolutions",
+              "count": 3
+            },
+            {
+              "access-method": "regular",
+              "metric-type": "unique-resolutions",
               "count": 3
             }
           ]
