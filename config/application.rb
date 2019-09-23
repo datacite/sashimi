1b0caf928fc52f55c8e7326dca0e622c12a3d32d
Martin Fenner <martin.fenner@datacite.org>
2019-09-23 18:42:48 +0200

    standardize logging. #87

--- config/application.rb
+++ config/application.rb
@@ -76,12 +76,13 @@ module Sashimi
     # secret_key_base is not used by Rails API, as there are no sessions
     config.secret_key_base = 'blipblapblup'
 
-    # configure logging
-    logger = ActiveSupport::Logger.new(STDOUT)
+    # Write all logs to STDOUT instead of file
+    logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
-    config.lograge.enabled = true
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
     config.log_level = ENV['LOG_LEVEL'].to_sym
+
+    config.active_job.logger = config.logger
 
     # configure caching
     config.cache_store = :dalli_store, nil, { :namespace => ENV['APPLICATION'] }
