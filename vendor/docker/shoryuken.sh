32210b27b24e4a5b99328faae3045e68ba5f66a9
Martin Fenner <martin.fenner@datacite.org>
2019-12-20 16:39:53 +0100

    don't load specific file for shoryuken

--- vendor/docker/shoryuken.sh
+++ vendor/docker/shoryuken.sh
@@ -2,5 +2,5 @@
 cd /home/app/webapp
 exec 2>&1
 if [ "$AWS_REGION" ]; then
-  exec /sbin/setuser app bundle exec shoryuken -R -C config/shoryuken.yml -r app/models/report_subset.rb
+  exec /sbin/setuser app bundle exec shoryuken -R -C config/shoryuken.yml
 fi
