477412739c9232f06d8d513236d310b648e29d88
kjgarza <kgarza@datacite.org>
2020-04-30 14:58:13 +0200

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,13 @@ Rails.application.routes.draw do
   resources :index, path: '/', only: [:index]
   # resources :index, only: [:index]
 
-  resources :publishers, constraints: { id: /.+/ }
-
   def add_swagger_route http_method, path, opts = {}
     full_path = path.gsub(/{(.*?)}/, ':\1')
     match full_path, to: "#{opts.fetch(:controller_name)}##{opts[:action_name]}", via: http_method
   end
 
-  add_swagger_route 'GET', '//reports/publishers', controller_name: 'publishers', action_name: 'index'
+  # add_swagger_route 'GET', '//reports/publishers', controller_name: 'publishers', action_name: 'index'
   add_swagger_route 'DELETE', '//publishers/{id}', controller_name: 'publishers', action_name: 'destroy'
-  add_swagger_route 'GET', '//repositories-usage-reports/{id}', controller_name: 'publishers', action_name: 'show'
   add_swagger_route 'PUT', '//publishers/{id}', controller_name: 'publishers', action_name: 'update'
   add_swagger_route 'POST', '//publishers', controller_name: 'publishers', action_name: 'create'
   add_swagger_route 'GET', '//report-types', controller_name: 'report_types', action_name: 'index'
@@ -25,4 +22,10 @@ Rails.application.routes.draw do
   add_swagger_route 'GET', '//report-subsets/{id}', controller_name: 'report_subsets', action_name: 'show'
   add_swagger_route 'PUT', '//reports/{id}', controller_name: 'reports', action_name: 'update'
   add_swagger_route 'POST', '//reports', controller_name: 'reports', action_name: 'create'
+
+  get 'repositories-usage-reports/:id', :to => 'publishers#show', constraints: { :id => /.+/ }
+
+
+  resources :publishers, constraints: { :id => /.+/ }, format: false, defaults: { format: false }
+  resources :repositories_usage_reports, constraints: { :id => /.+/ }, format: false, defaults: { format: false }
 end
