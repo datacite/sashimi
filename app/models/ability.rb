32bd92e24bc6c0c3fc85b129c30075e33ba2bd79
kjgarza <kgarza@datacite.org>
2020-07-09 12:07:18 +0200

    test: add abilities test

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,8 +9,6 @@ class Ability
 
     if user.role_id == "staff_admin"
       can :manage, :all
-    elsif user.role_id == "provider_admin" && user.provider_id.present?
-      can [:create, :update, :read], Report, :provider_id => user.provider_id
     elsif user.role_id == "client_admin" && user.client_id.present?
       can [:create, :update, :read], Report, :client_id => user.client_id
     end
---
+++ spec/models/abilities_spec.rb
@@ -0,0 +1,53 @@
+require "rails_helper"
+require "cancan/matchers"
+
+describe User, type: :model do
+  let(:token) { User.generate_token }
+  let(:user) { User.new(token) }
+  let(:report) { create(:report, client_id: "datacite.datacite") }
+  let(:report_subset) { create(:report_subset, report_id: report.uid) }
+
+  let(:report_cdl) { create(:report, client_id: "cdl.dash") }
+  let(:report_subset_cdl) { create(:report_subset, report_id: report_cdl.uid) }
+
+  describe "User attributes", order: :defined do
+    it "is valid with valid attributes" do
+      expect(user.name).to eq("Josiah Carberry")
+    end
+  end
+
+  describe "abilities", vcr: true do
+    subject { Ability.new(user) }
+
+    context "when is a staff_admin" do
+      let(:token){ User.generate_token(role_id: "staff_admin") }
+
+      it { is_expected.to be_able_to(:read, user) }
+      it { is_expected.to be_able_to(:read, report) }
+      it { is_expected.to be_able_to(:create, report) }
+      it { is_expected.to be_able_to(:update, report) }
+      it { is_expected.to be_able_to(:destroy, report) }
+    end
+
+    context "when is a client admin" do
+      let(:token){ User.generate_token(role_id: "client_admin", client_id: "datacite.datacite") }
+
+      it { is_expected.not_to be_able_to(:read, user) }
+
+      it { is_expected.to be_able_to(:read, report) }
+      it { is_expected.to be_able_to(:create, report) }
+      it { is_expected.to be_able_to(:update, report) }
+      it { is_expected.not_to be_able_to(:destroy, report) }
+
+      it { is_expected.not_to be_able_to(:read, report_cdl) }
+      it { is_expected.not_to be_able_to(:create, report_cdl) }
+      it { is_expected.not_to be_able_to(:update, report_cdl) }
+      it { is_expected.not_to be_able_to(:destroy, report_cdl) }
+
+      # it { is_expected.to be_able_to(:read, report_subset) }
+      # it { is_expected.to be_able_to(:create, report_subset) }
+      # it { is_expected.to be_able_to(:update, report_subset) }
+      # it { is_expected.not_to be_able_to(:destroy, report_subset) }
+    end
+  end
+end
