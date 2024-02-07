9753159b5fc2727a8eaec0785ba174fe71443a2d
Richard Hallett <richard@richardhallett.com>
2024-02-07 15:09:07 +0100

    Fix sushi validation error syntax
    use #add method not <<

--- app/validators/sushi_validator.rb
+++ app/validators/sushi_validator.rb
@@ -2,20 +2,19 @@ class SushiValidator < ActiveModel::EachValidator
 
   def validate_each(record, _schema, _value)
     valid = record.validate_sushi
-    unless valid == true or valid == []
-
+    unless valid == true || valid == []
       def dig_errors(errors)
         return [] if errors.nil?
 
         if errors.is_a? Hash
-          [ errors.dig(:message) ]
+          [errors.dig(:message)]
         else
-          errors.map { |error| {error.dig(:fragment) => error.dig(:message)}  }
+          errors.map { |error| { error.dig(:fragment) => error.dig(:message) } }
         end
       end
 
       nice_errors = dig_errors(valid)
-      record.errors["errors"] << (nice_errors || "Your SUSHI is wrong mate!!")
+      record.errors.add(:base, nice_errors || "Your SUSHI is wrong mate!!")
     end
   end
 end
