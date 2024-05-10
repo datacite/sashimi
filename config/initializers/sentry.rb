bbaf8e3f36acbaee8aeea2c0dd9ac9ac530021b4
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2024-05-10 18:06:17 +0200

    add reporting environment to sentry raven config

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Raven.configure do |config|
+  config.environments = %w(stage production)
   config.dsn = ENV["SENTRY_DSN"]
   config.release = "sashimi:" + Sashimi::Application::VERSION
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
