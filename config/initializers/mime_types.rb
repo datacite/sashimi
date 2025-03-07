aa5748e4d389e2b802f9da5aeea4e2e6dfff4569
codycooperross <50597551+codycooperross@users.noreply.github.com>
2025-03-07 15:19:45 -0500

    Include other application/json synonyms

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,6 +3,8 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 json_mime_type_synonyms = %w[
+  text/x-json
+  application/jsonrequest
   gzip
   application/gzip
 ]
